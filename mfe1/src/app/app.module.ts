4edee080ea8d2d8211d3d4cb754542d2eca0b1e2
liyxb <liyxb@digiwin.com>
2024-04-01 18:18:28 +0800

    ✨ feat: 动画

--- mfe1/src/app/app.module.ts
+++ mfe1/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { MicrofrontendModule } from './microfrontend/microfrontend.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MicrofrontendModule
+    MicrofrontendModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mfe1/src/app/microfrontend/task/list/list.component.ts
+++ mfe1/src/app/microfrontend/task/list/list.component.ts
@@ -1,27 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { slideInAnimation } from '../animations';
+import { animate, group, query, stagger, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'remote-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  animations: [
+    trigger('pageAnimations', [
+      transition(':enter', [
+        query('.task-card', [
+          style({opacity: 1, transform: 'translateY(1000px)'}),
+          stagger(30, [
+            animate('500ms cubic-bezier(0, 0, 0.5 ,1)',
+            style({ opacity: 1, transform: 'translateY(0px)' }))
+          ])
+        ])
+      ])
+    ]),
+    trigger('filterAnimation', [
+      transition(':enter, * => 0, * => -1', []),
+    ]),
+  ]
 })
 export class ListComponent implements OnInit {
+  @HostBinding('@pageAnimations')
+  public animatePage = true;
+  heroesTotal = -1;
   tasks = [
-    { title: 'Task 1', description: 'Description for Task 1',id:'1' },
-    { title: 'Task 2', description: 'Description for Task 2' ,id:'2'},
+    { title: 'Task 1', description: 'Description for Task 1', id: '1' },
+    { title: 'Task 2', description: 'Description for Task 2', id: '2' },
   ];
 
   constructor(
-    private route: ActivatedRoute,  
+    private route: ActivatedRoute,
     private router: Router,
   ) { }
 
   ngOnInit(): void {
-    
+
   }
 
-  goToDetail(id:string):void {
-    this.router.navigate(['./detail'],{relativeTo:this.route})
+  goToDetail(id: string): void {
+    this.router.navigate(['./detail'], { relativeTo: this.route })
   }
 }
